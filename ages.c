6ee84b8778df75db2b28cff7b3a4cd4785c3d345
your_github_username <richesuche91@gmail.com>
2023-11-09 15:20:08 -0800

    familia

---
+++ ages.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int ages[] = {60, 35, 30, 26, 22, 18};
+	int i;
+
+	for (i = 0; i < 6; i++) {
+		printf("AGES: %d\n", ages[i]);
+	}
+
+	return (0);
+}
---
+++ familia.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("My family is made up of one girl and four boys.\n");
+	printf("Names of the boys are:\n");
+	printf("NAMES: %s\n", "CHIEF\n, AIR MARSHAL\n, PABLO de ESCOBAR\n, RICHES\n");
+	printf("The girl name is:\n");
+	printf("NAME: %s\n", "BLAQ DIAMOND");
+	printf("my Mother:\n");
+	printf("NAME: %s\n", "MI AMOR");
+
+	printf("MI FAMILIA ES DIVERTIDA.\n");
+
+	return (0);
+}
